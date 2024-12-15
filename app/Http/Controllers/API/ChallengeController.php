725994b811290da6aefc7ee0ab4b71f0202ed1c5
egido-quan <isidro.zapater@gmail.com>
2024-12-15 14:42:19 +0100

    Challenges show by player added

--- app/Http/Controllers/API/ChallengeController.php
+++ app/Http/Controllers/API/ChallengeController.php
@@ -34,20 +34,20 @@ class ChallengeController extends Controller
 
         $message = "OK";
 
-        if (self::samePlayer($player1->ranking, $player2->ranking))
+        if (self::isSamePlayer($player1->ranking, $player2->ranking))
             $message = "A player cannot challenge herself/himself";
 
-        elseif (!self::rankgingGapOk($player1->ranking, $player2->ranking))
-            $message =  "Ranking gap between players must be 3 or higher";
+        elseif (!self::isRankgingGapOk($player1->ranking, $player2->ranking))
+            $message =  "Ranking gap between players must be 3 or lower";
 
-        elseif (!self::setScoreOK($request->score['player1_set1'], $request->score['player2_set1']))
+        elseif (!self::isSetScoreOK($request->score['player1_set1'], $request->score['player2_set1']))
             $message = "1st set score is wrong";
 
-        elseif (!self::setScoreOK($request->score['player1_set2'], $request->score['player2_set2']))
+        elseif (!self::isSetScoreOK($request->score['player1_set2'], $request->score['player2_set2']))
             $message = "2nd set score is wrong";
     
         elseif (isset($request->score['player1_set3']) || isset($request->score['player2_set3'])) {
-            if (!self::setScoreOK($request->score['player1_set3'], $request->score['player2_set3']))
+            if (!self::isSetScoreOK($request->score['player1_set3'], $request->score['player2_set3']))
                 $message = "3rd set score is wrong";
         }
 
@@ -78,21 +78,23 @@ class ChallengeController extends Controller
             $player2->save();
          }
 
-        return response()->json([ $player1_ranking, $player1->ranking, $player2_ranking, $player2->ranking]);
-
         return response()->json([
             'response_code'     => '200',
             'status'            => 'success',
             'message'           => 'Challenge registration successful',
             'player1_user_id'   => $challenge->player1_user_id,
+            'player1_old_ranking'    => $player1_ranking,
+            'player1_new_ranking'    => $player1->ranking,
             'player2_user_id'   => $challenge->player2_user_id,
+            'player2_old_ranking'    => $player2_ranking,
+            'player2_new_ranking'    => $player2->ranking,
             'score'             => $challenge->score,
 
         ]);
 
     }
 
-    protected function samePlayer($p1_ranking, $p2_ranking) {
+    protected function isSamePlayer($p1_ranking, $p2_ranking) {
         if ($p1_ranking === $p2_ranking) {
             return true;
         } else {
@@ -100,7 +102,7 @@ class ChallengeController extends Controller
         }
     }
 
-    protected function rankgingGapOk($p1_ranking, $p2_ranking) {
+    protected function isRankgingGapOk($p1_ranking, $p2_ranking) {
         if (abs($p1_ranking - $p2_ranking) > 3) {
             return false;
         } else {
@@ -108,7 +110,7 @@ class ChallengeController extends Controller
         }
     }
 
-    protected function setScoreOK($p1_set_score, $p2_set_score) {
+    protected function isSetScoreOK($p1_set_score, $p2_set_score) {
         if (($p1_set_score == 7 && $p2_set_score == 7) || 
             (($p1_set_score == 7 || $p2_set_score == 7) && abs($p1_set_score - $p2_set_score) > 2) ||
             (($p1_set_score < 7 && $p2_set_score < 7) && abs($p1_set_score - $p2_set_score) < 2) ||
@@ -118,8 +120,6 @@ class ChallengeController extends Controller
             return true;
         }
     }
-
-
 
     protected function ganador($score) {
         $jug1 = 0;
@@ -134,4 +134,25 @@ class ChallengeController extends Controller
         }
         return ($jug1>$jug2) ? 1 : 2;
     }
+
+    public function show($player_id) {
+
+        $player = Player::find($player_id);
+        $challengesList = $player
+        ->allChallenges();
+        //->with('challenges_1') //Hay que comprobar si el jugador está en el campo player_1 o player_2
+        //->with('challenges_2')
+        //->first();
+
+        return response()->json([
+            'response_code'     => '200',
+            'status'            => 'success',
+            'message'           => 'Challenge list successful',
+            'challenges'        => $challengesList
+
+        ]);
+    }
+
+
+   
 }
--- tests/Feature/ChallengeTest.php
+++ tests/Feature/ChallengeTest.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\User;
 use App\Models\Challenge;
+use App\Models\Player;
 
 class ChallengeTest extends TestCase
 {
@@ -15,7 +16,7 @@ class ChallengeTest extends TestCase
      */
 
 /** @test */
-   public function players_2_and_3_challenge_can_be_registered() { 
+   public function players_id_2_and_id_3_challenge_can_be_registered() { 
    
     $this->withoutExceptionHandling();
 
@@ -24,14 +25,14 @@ class ChallengeTest extends TestCase
 
     $player1_user_id = 2;
     $player2_user_id = 3;
-    $score = [
+    $score = ([
         'player1_set1' => 6,
         'player2_set1' => 3,
         'player1_set2' => 4,
         'player2_set2' => 6,
         'player1_set3' => 6,
         'player2_set3' => 2
-    ];
+    ]);
 
     $response = $this->post('/api/register_challenge',
     [
@@ -60,13 +61,76 @@ class ChallengeTest extends TestCase
 
     $this->assertEquals($challengeInfo['player1_user_id'], $player1_user_id);
     $this->assertEquals($challengeInfo['player2_user_id'], $player2_user_id);
-    $this->assertEquals($challengeInfo['score'], $score);
-
-
-
-
+    $this->assertEquals($challengeInfo['score'], json_encode($score));
     }
 
+    
+    /** @test */
+   public function ranking_4_5_are_swapped_if_challenge_is_won() { 
+   
+    $this->withoutExceptionHandling();
+
+    $user = User::where('email', "admin@admin")->first();
+    $userAdminToken = $user->createToken('admin')->accessToken;
+
+    $player1 = Player::where('ranking',5)->first();
+    $player2 = Player::where('ranking',4)->first();
+
+    $score = ([
+        'player1_set1' => 6,
+        'player2_set1' => 3,
+        'player1_set2' => 4,
+        'player2_set2' => 6,
+        'player1_set3' => 6,
+        'player2_set3' => 2
+    ]);
+
+    $response = $this->post('/api/register_challenge',
+    [
+        'player1_user_id' => $player1->user_id,
+        'player2_user_id' => $player2->user_id,
+        'score' => $score
+    ],
+    [
+        'Authorization' => 'Bearer ' . $userAdminToken
+    ]);
+
+
+
+    $responseJson = $response->json();
+    $this->assertEquals($responseJson['response_code'], '200');
+    $this->assertEquals($responseJson['message'], 'Challenge registration successful');
+
+    $challenge = Challenge::all()->last();
+
+    $player1 = Player::where('user_id',$player1->user_id)->first();
+    $player2 = Player::where('user_id',$player2->user_id)->first();
+
+    $this->assertEquals(4, $player1->ranking);
+    $this->assertEquals(5, $player2->ranking);
+    }
+
+     /** @test */
+   public function can_show_player_id_3_matches() { 
+   
+    //$this->withoutExceptionHandling();
+
+    $user = User::where('email', "jannik@sinner")->first();
+    $userToken = $user->createToken('user')->accessToken;
+
+    $player1 = Player::where('user_id',3)->first();
+
+    $response = $this->get('/api/challenge/3',
+    [
+        'Authorization' => 'Bearer ' . $userToken
+    ]);
+
+
+    $responseJson = $response->json();
+    $this->assertEquals($responseJson['response_code'], '200');
+    $this->assertEquals($responseJson['message'], 'Challenge list successful');
+
+    }
 
 
 }
